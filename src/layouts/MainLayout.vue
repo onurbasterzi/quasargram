1596b64cb668a3ea85156c27f0a273ba2c70963e
Ahmet Onur  Başterzi <ahmet@onurbasterzi.com>
2020-10-22 21:29:11 +0300

    first commit for

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-footer class="bg-white" bordered>
+
+   <q-tabs
+         class="text-grey-10"
+         active-color="primary"
+         indicator-color="transparent"
+       >
+      <q-route-tab
+        to='/'
+        icon="mail"
+        label="Home"
+        name="home"
+      />
+       <q-route-tab
+          to='/camera'
+          icon="alarm"
+          label="Camera"
+          name="camera"
+        />
+
+      </q-tabs>
+
+    </q-footer>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
