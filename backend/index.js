aa586b64f449340d041944761b3612c201adbee8
Ahmet Onur  Başterzi <ahmet@onurbasterzi.com>
2020-10-25 15:07:11 +0300

    updating heroku

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,43 @@
 
 const express = require('express')
 
+const admin = require('firebase-admin');
+
+
+
 /* config */
 const app = express()
 
+/* config firebase*/
+const serviceAccount = require('./serviceAccountKey.json');
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
 
 /* endpoint -posts */
 
 app.get('/posts', (request, response) => {
-  let posts=[
-    {
-      caption:'Efes Anfi Tiyatrosu',
-      location:'Selçuk',
-    },
-    {
-      caption:'Celcus Kütüphanesi',
-      location:'Efes Selçuk',
-    }
-  ]
-  response.send(posts)
-  console.log("Endpoint is working :)")
+  response.set('Access-Control-Allow-Origin','*')
+
+  let posts = []
+  db.collection('posts').orderBy('date','desc').get().then(snapshot=>{
+    snapshot.forEach((doc) => {
+      //console.log(doc.id, '=>', doc.data());
+      posts.push(doc.data())
+    });
+    response.send(posts)
+  })
+})
+
+
+/* endpoint -cereatePosts */
+
+app.get('/createPost', (request, response) => {
+  response.set('Access-Control-Allow-Origin','*')
+  response.send('createPost')
+
 })
 
 /* listen */
